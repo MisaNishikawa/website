c29d0a960b0aac94c5d3fd76050b7786e7399c0b
西川未紗 <>
2020-01-31 01:35:26 +0800

    website

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+
+// topに戻るボタン
+$(function(){
+    var pagetop = $('#page_top');
+    // ボタン非表示
+    pagetop.hide();
+  
+    // 100px スクロールしたらボタン表示
+    $(window).scroll(function () {
+       if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+       } else {
+            pagetop.fadeOut();
+       }
+    });
+    pagetop.click(function () {
+       $('body, html').animate({ scrollTop: 0 }, 500);
+       return false;
+    });
+  });
+
+  
+  //スライドショー
+  $(function(){
+        $("img.ChangePhoto").click(function(){
+            var ImgSrc = $(this).attr("src");
+            var ImgAlt = $(this).attr("alt");
+            $("img#MainPhoto").attr({src:ImgSrc,alt:ImgAlt});
+            $("img#MainPhoto").hide();
+            $("img#MainPhoto").fadeIn("slow");
+            return false;
+        }); 
+    });
+    
